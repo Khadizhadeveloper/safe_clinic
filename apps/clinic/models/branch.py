79269199c5518207b106c21939df18423ac26fa9
Khadizhadeveloper <hadyousha6@gmail.com>
2024-10-23 22:14:54 +0600

    Director was added

--- apps/clinic/models/branch.py
+++ apps/clinic/models/branch.py
@@ -1,9 +1,19 @@
 from django.db import models
+class Director(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
 
 class Branch(models.Model):
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
     phone = models.CharField(max_length=255)
-    director=models.ManyToManyField('Director')
+    director=models.ForeignKey(Director, on_delete=models.CASCADE)
     def __str__(self):
         return self.name
+
