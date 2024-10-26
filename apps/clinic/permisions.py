41c1209aff3d30b8c15e85d1d96e6fecfe829d14
Khadizhadeveloper <hadyousha6@gmail.com>
2024-10-26 20:40:11 +0600

    IsAdminUser, IsDoctorUser, IsDirectorUser permissions were created

---
+++ apps/clinic/permisions.py
@@ -0,0 +1,21 @@
+from rest_framework.permissions import BasePermission
+
+class IsAdminUser(BasePermission):
+    def has_permission(self, request, view):
+        # Проверка, что пользователь аутентифицирован и его роль - администратор
+        return request.user.is_authenticated and request.user.role == 'admin'
+
+
+class IsDoctorUser(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'doctor'
+
+
+class IsDirectorUser(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'director'
+
+
+class IsPatientUser(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'patient'
