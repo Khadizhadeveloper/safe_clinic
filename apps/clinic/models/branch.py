9ac41e886629d3ef91b0f3150886494219f7229d
Khadizhadeveloper <hadyousha6@gmail.com>
2024-11-04 23:52:46 +0600

    Small fix

--- apps/clinic/models/branch.py
+++ apps/clinic/models/branch.py
@@ -1,20 +1,20 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
 class Director(models.Model):
-    name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
-    phone = models.CharField(max_length=255)
-    birth_date = models.DateField()
+    name = models.CharField(max_length=255, verbose_name=_('Name'))
+    address = models.CharField(max_length=255, verbose_name=_('Address'))
+    phone = models.CharField(max_length=255, verbose_name=_('Phone'))
+    birth_date = models.DateField(verbose_name=_('Birth date'))
 
     def __str__(self):
         return self.name
 
 
 class Branch(models.Model):
-    name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
-    phone = models.CharField(max_length=255)
-    director=models.ForeignKey(Director, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, verbose_name=_('Name'))
+    address = models.CharField(max_length=255, verbose_name=_('Address'))
+    phone = models.CharField(max_length=255, verbose_name=_('Phone'))
+    director=models.ForeignKey(Director, on_delete=models.CASCADE, verbose_name=_('Director'))
     def __str__(self):
         return self.name
 
